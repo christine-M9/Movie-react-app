84be558f3bcbc56e0121a3c2a4d3d68fdd04dc7b
Christine <christinekhekama99@gmail.com>
2023-07-27 01:26:57 +0300

    css , minor changes

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 const App = () => {
   const [films, setFilms] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    description: '',
+    runtime: '',
+    showtime: '',
+    tickets_sold: '',
+    poster: '',
+  });
 
   useEffect(() => {
     fetch('/db.json')
@@ -16,10 +24,33 @@ const App = () => {
     setSelectedMovie(movie);
   };
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewMovie({
+      ...newMovie,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const id = films.length + 1;
+    const newMovieWithId = { ...newMovie, id: id.toString() };
+    setFilms([...films, newMovieWithId]);
+    setNewMovie({
+      title: '',
+      description: '',
+      runtime: '',
+      showtime: '',
+      tickets_sold: '',
+      poster: '',
+    });
+  };
+
   return (
     <div className="App">
       <header>
-        <h1>Movie List</h1>
+        <h1>MOVIE THEATRE</h1>
       </header>
       <main>
         <div className="container">
@@ -34,28 +65,58 @@ const App = () => {
               <button onClick={() => setSelectedMovie(null)}>Back to List</button>
             </div>
           ) : (
-            <table>
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Description</th>
-                  <th>Runtime</th>
-                  <th>Showtime</th>
-                  <th>Tickets Sold</th>
-                </tr>
-              </thead>
-              <tbody>
-                {films.map((film) => (
-                  <tr key={film.id} onClick={() => handleMovieClick(film)}>
-                    <td>{film.title}</td>
-                    <td>{film.description}</td>
-                    <td>{film.runtime} mins</td>
-                    <td>{film.showtime}</td>
-                    <td>{film.tickets_sold}</td>
+            <React.Fragment>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Runtime</th>
+                    <th>Showtime</th>
+                    <th>Tickets Sold</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {films.map((film) => (
+                    <tr key={film.id} onClick={() => handleMovieClick(film)}>
+                      <td>{film.title}</td>
+                      <td>{film.description}</td>
+                      <td>{film.runtime} mins</td>
+                      <td>{film.showtime}</td>
+                      <td>{film.tickets_sold}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <form className="add-movie-form" onSubmit={handleFormSubmit}>
+                <h2>Add New Movie</h2>
+                <label>
+                  Title:
+                  <input type="text" name="title" value={newMovie.title} onChange={handleInputChange} required />
+                </label>
+                <label>
+                  Description:
+                  <textarea name="description" value={newMovie.description} onChange={handleInputChange} required />
+                </label>
+                <label>
+                  Runtime:
+                  <input type="text" name="runtime" value={newMovie.runtime} onChange={handleInputChange} required />
+                </label>
+                <label>
+                  Showtime:
+                  <input type="text" name="showtime" value={newMovie.showtime} onChange={handleInputChange} required />
+                </label>
+                <label>
+                  Tickets Sold:
+                  <input type="number" name="tickets_sold" value={newMovie.tickets_sold} onChange={handleInputChange} required />
+                </label>
+                <label>
+                  Poster URL:
+                  <input type="url" name="poster" value={newMovie.poster} onChange={handleInputChange} required />
+                </label>
+                <button type="submit">Add Movie</button>
+              </form>
+            </React.Fragment>
           )}
         </div>
       </main>
@@ -64,3 +125,5 @@ const App = () => {
 };
 
 export default App;
+
+
