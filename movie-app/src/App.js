4398ef8428f5c6325ad89b69faa425ff784c2ac4
Christine <christinekhekama99@gmail.com>
2023-07-27 17:12:28 +0300

    routing

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
 
 const App = () => {
   const [films, setFilms] = useState([]);
@@ -47,79 +49,85 @@ const App = () => {
     });
   };
 
+  let maindata = <main>
+  <div className="container">
+    {selectedMovie ? (
+      <div className="selected-movie">
+        <img src={selectedMovie.poster} alt={selectedMovie.title} />
+        <h2>{selectedMovie.title}</h2>
+        <p>{selectedMovie.description}</p>
+        <p>Runtime: {selectedMovie.runtime} mins</p>
+        <p>Showtime: {selectedMovie.showtime}</p>
+        <p>Tickets Sold: {selectedMovie.tickets_sold}</p>
+        <button onClick={() => setSelectedMovie(null)}>Back to List</button>
+      </div>
+    ) : (
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Runtime</th>
+              <th>Showtime</th>
+              <th>Tickets Sold</th>
+            </tr>
+          </thead>
+          <tbody>
+            {films.map((film) => (
+              <tr key={film.id} onClick={() => handleMovieClick(film)}>
+                <td>{film.title}</td>
+                <td>{film.description}</td>
+                <td>{film.runtime} mins</td>
+                <td>{film.showtime}</td>
+                <td>{film.tickets_sold}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <form className="add-movie-form" onSubmit={handleFormSubmit}>
+          <h2>Add New Movie</h2>
+          <label>
+            Title:
+            <input type="text" name="title" value={newMovie.title} onChange={handleInputChange} required />
+          </label>
+          <label>
+            Description:
+            <textarea name="description" value={newMovie.description} onChange={handleInputChange} required />
+          </label>
+          <label>
+            Runtime:
+            <input type="text" name="runtime" value={newMovie.runtime} onChange={handleInputChange} required />
+          </label>
+          <label>
+            Showtime:
+            <input type="text" name="showtime" value={newMovie.showtime} onChange={handleInputChange} required />
+          </label>
+          <label>
+            Tickets Sold:
+            <input type="number" name="tickets_sold" value={newMovie.tickets_sold} onChange={handleInputChange} required />
+          </label>
+          <label>
+            Poster URL:
+            <input type="url" name="poster" value={newMovie.poster} onChange={handleInputChange} required />
+          </label>
+          <button type="submit">Add Movie</button>
+        </form>
+      </>
+    )}
+  </div>
+</main>
+
   return (
     <div className="App">
       <header>
         <h1>MOVIE THEATRE</h1>
       </header>
-      <main>
-        <div className="container">
-          {selectedMovie ? (
-            <div className="selected-movie">
-              <img src={selectedMovie.poster} alt={selectedMovie.title} />
-              <h2>{selectedMovie.title}</h2>
-              <p>{selectedMovie.description}</p>
-              <p>Runtime: {selectedMovie.runtime} mins</p>
-              <p>Showtime: {selectedMovie.showtime}</p>
-              <p>Tickets Sold: {selectedMovie.tickets_sold}</p>
-              <button onClick={() => setSelectedMovie(null)}>Back to List</button>
-            </div>
-          ) : (
-            <React.Fragment>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Runtime</th>
-                    <th>Showtime</th>
-                    <th>Tickets Sold</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {films.map((film) => (
-                    <tr key={film.id} onClick={() => handleMovieClick(film)}>
-                      <td>{film.title}</td>
-                      <td>{film.description}</td>
-                      <td>{film.runtime} mins</td>
-                      <td>{film.showtime}</td>
-                      <td>{film.tickets_sold}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              <form className="add-movie-form" onSubmit={handleFormSubmit}>
-                <h2>Add New Movie</h2>
-                <label>
-                  Title:
-                  <input type="text" name="title" value={newMovie.title} onChange={handleInputChange} required />
-                </label>
-                <label>
-                  Description:
-                  <textarea name="description" value={newMovie.description} onChange={handleInputChange} required />
-                </label>
-                <label>
-                  Runtime:
-                  <input type="text" name="runtime" value={newMovie.runtime} onChange={handleInputChange} required />
-                </label>
-                <label>
-                  Showtime:
-                  <input type="text" name="showtime" value={newMovie.showtime} onChange={handleInputChange} required />
-                </label>
-                <label>
-                  Tickets Sold:
-                  <input type="number" name="tickets_sold" value={newMovie.tickets_sold} onChange={handleInputChange} required />
-                </label>
-                <label>
-                  Poster URL:
-                  <input type="url" name="poster" value={newMovie.poster} onChange={handleInputChange} required />
-                </label>
-                <button type="submit">Add Movie</button>
-              </form>
-            </React.Fragment>
-          )}
-        </div>
-      </main>
+      <Routes>
+        <Route path='/show' element={maindata}/>
+        <Route path='/about' element={<h1>THis is about us</h1>}/>
+      </Routes>
+      
     </div>
   );
 };
